d30c4ace4c773218725223650a5608da8b0c899c
PetarRan <petarrandjelovic66@gmail.com>
2025-05-01 00:12:15 +0200

    chore:add dotenv and cors, package update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
