8b96dde8b604da1fb16dbec430abb175de558c45
AlessandroDodi <dodialessandro3@gmail.com>
2025-05-14 12:32:18 +0200

    Refactor chat

---
+++ client/src/app/dashboard/components/buttons/SendButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { IconButton, Box } from "@mui/material";
+import SendIcon from "../icons/SendIcon";
+
+interface SendButtonProps {
+  onClick: () => void;
+}
+
+const SendButton: React.FC<SendButtonProps> = ({ onClick }) => (
+  <IconButton
+    color="primary"
+    onClick={onClick}
+    sx={{
+      bgcolor: "#ecf5fe",
+      "&:hover": {
+        bgcolor: "#d9edfd",
+      },
+      width: 36,
+      height: 36,
+    }}
+  >
+    <Box component="span" sx={{ width: 16, height: 16, display: "flex" }}>
+      <SendIcon />
+    </Box>
+  </IconButton>
+);
+
+export default SendButton;
---
+++ client/src/app/dashboard/components/buttons/SourcesButton.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "@mui/material";
+import SourcesIcon from "../icons/SourcesIcon";
+
+interface SourcesButtonProps {
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
+const SourcesButton: React.FC<SourcesButtonProps> = ({
+  onClick,
+  disabled = true,
+}) => (
+  <Button
+    disabled={disabled}
+    onClick={onClick}
+    startIcon={<SourcesIcon />}
+    sx={{
+      color: "#555555",
+      textTransform: "none",
+      borderRadius: 2,
+      fontWeight: 500,
+      fontSize: "0.875rem",
+      py: 0.5,
+      px: 1.5,
+      minWidth: "auto",
+      "&:hover": {
+        backgroundColor: "#f1f1f1",
+      },
+    }}
+  >
+    Sources
+  </Button>
+);
+
+export default SourcesButton;
